4ae7280936369129a65c9c1adb1c2bb651e805d8
Alex Pitenko <oleksii.p@itsyndicate.org>
2025-05-31 15:50:43 +0200

    Removed empty migration

--- db/migrations/1748689847547-claimed-free-messages.ts
+++ db/migrations/1748689847547-claimed-free-messages.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migrations1748689847547 implements MigrationInterface {
-    name = 'Migrations1748689847547'
+export class ClaimedFreeMessagesMigrations1748689847547 implements MigrationInterface {
+    name = 'ClaimedFreeMessagesMigrations1748689847547'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "user" ADD "claimedFreeMessages" boolean NOT NULL DEFAULT false`);
